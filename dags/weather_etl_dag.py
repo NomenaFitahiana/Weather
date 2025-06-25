672284805ecbeb4722e5bf9160c30add11d399d4
NomenaFitahiana <hei.nomena.3@gmail.com>
2025-06-26 00:21:24 +0300

    feat: create the dag for extract and transform

---
+++ dags/weather_etl_dag.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import os
+
+# Définir les fonctions Python qui appellent tes scripts
+def extract_data():
+    os.system("python scripts/extract.py")
+
+def transform_data():
+    os.system("python scripts/transform.py")
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2025, 6, 26),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+with DAG(
+    dag_id="weather_etl_dag",
+    default_args=default_args,
+    schedule_interval="0 6 * * *",  # tous les jours à 6h du matin
+    catchup=False,
+    description="Extraction et transformation météo",
+) as dag:
+
+    extract = PythonOperator(
+        task_id="extract_weather_data",
+        python_callable=extract_data
+    )
+
+    transform = PythonOperator(
+        task_id="transform_weather_data",
+        python_callable=transform_data
+    )
+
+    extract >> transform
